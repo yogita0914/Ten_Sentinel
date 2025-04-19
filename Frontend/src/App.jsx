ecedc5e05c08c72808b2f7591fde44f7eb4aae53
Abhinav Sharma <114858652+AbhinavSharma486@users.noreply.github.com>
2025-04-19 13:06:37 +0530

    adding tailwindcss completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,9 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <h1>Ten Sentinel</h1>
+      <h1 className="text-3xl font-bold underline text-purple-500">
+        Ten Sentinel
+      </h1>
     </div>
   );
 };
