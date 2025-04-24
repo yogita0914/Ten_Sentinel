e8ee1a0139f2d1e739bdfc1b3c4e8ab7bca4251c
yogita0914 <yogitazee1@gmail.com>
2025-04-24 10:44:22 +0530

    added career

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import LandingPage from './pages/LandingPage';
